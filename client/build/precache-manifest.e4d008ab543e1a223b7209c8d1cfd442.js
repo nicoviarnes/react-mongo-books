f755fb278b55fd73a9e711021549901a8b5496fe
nicoviarnes <nlviarnes@gmail.com>
2019-05-02 19:39:38 -0700

    add to heroku

---
+++ client/build/precache-manifest.e4d008ab543e1a223b7209c8d1cfd442.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f4744b0874a7d14cd59b",
+    "url": "/static/js/main.6ba7c550.chunk.js"
+  },
+  {
+    "revision": "19f0dc92cc37bea57c9d",
+    "url": "/static/js/2.4d4d1d51.chunk.js"
+  },
+  {
+    "revision": "f4744b0874a7d14cd59b",
+    "url": "/static/css/main.70674077.chunk.css"
+  },
+  {
+    "revision": "31c0099dd6282e8d964413d4dd979b7f",
+    "url": "/index.html"
+  }
+];
---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/books">Books</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
